7d0af80ecf792bfb90801465bf0d1899887d4cfc
Sean Delaney <seandarren96@gmail.com>
2020-11-06 23:32:55 -0500

    completed view and update

--- schema.sql
+++ schema.sql
@@ -2,32 +2,24 @@
 
 DROP DATABASE IF EXISTS employee_appDB;
 CREATE DATABASE employee_appDB;
-
 USE employee_appDB;
-
-/* Create new table for role with a primary key that auto-increments, and a text field */
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(7,2),
-    department_id INT,
-    PRIMARY KEY (id)
-
-); 
-
-
-/* Create new table for employee with a primary key that auto-increments, and a text field */
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-   last_name VARCHAR(30) NOT NULL,  
-  role_id INT,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
   manager_id INT NULL,
   PRIMARY KEY (id)
 );
-
- CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30), 
-    PRIMARY KEY (id)
-  );
